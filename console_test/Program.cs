b325544f4e384afb8ac83f107c262e6d7d3af6de
Jose Medrano <jmedrano87@users.noreply.github.com>
2016-03-25 11:45:40 -0700

    Implemented some exception handling.

--- console_test/Program.cs
+++ console_test/Program.cs
@@ -10,6 +10,7 @@ namespace apod_api
             APOD_API apod_api = new APOD_API();
             int action;
             DateTime oldestDate = new DateTime(1995, 06, 25);
+            WebClient dl = new WebClient();
 
             do
             {
@@ -24,7 +25,6 @@ namespace apod_api
                 Console.Write("Enter selection: ");
 
                 action = int.Parse(Console.ReadLine());
-                WebClient dl = new WebClient();
 
                 switch (action)
                 {
@@ -33,13 +33,25 @@ namespace apod_api
                         break;
                     case 1:
                         Console.WriteLine("Downloading standard photo . . .");
-                        dl.DownloadFile(apod_api.apod.url, "apod_" + apod_api.apod.date + ".jpg");
-                        Console.WriteLine("Finished downloading.");
+                        try {
+                            dl.DownloadFile(apod_api.apod.url, "apod_"
+                                + apod_api.apod.date.ToString("yyyy-MM-dd") + "_lq.jpg");
+                            Console.WriteLine("Finished downloading.");
+                        }
+                        catch (WebException ex) {
+                            Console.WriteLine(ex.Message);
+                        }
                         break;
                     case 2:
                         Console.WriteLine("Downloading hd photo . . .");
-                        dl.DownloadFile(apod_api.apod.hdurl, "apod_" + apod_api.apod.date + ".jpg");
-                        Console.WriteLine("Finished downloading.");
+                        try {
+                            dl.DownloadFile(apod_api.apod.hdurl, "apod_"
+                                + apod_api.apod.date.ToString("yyyy-MM-dd") + "_hq.jpg");
+                            Console.WriteLine("Finished downloading.");
+                        }
+                        catch (WebException ex) {
+                            Console.WriteLine(ex.Message);
+                        }
                         break;
                     case 3:
                         if (apod_api.apod.date.AddDays(-1) >= oldestDate)
