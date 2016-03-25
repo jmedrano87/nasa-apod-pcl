bcb4ee3a57d11607b5fb2c3d5cfcd4b82f1a6e74
Jose Medrano <jmedrano87@users.noreply.github.com>
2016-03-24 20:47:38 -0700

    Added ability to download photo in console app.

--- console_test/Program.cs
+++ console_test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace apod_api
 {
@@ -12,7 +13,35 @@ namespace apod_api
 
             Console.WriteLine("Title: " + apod_api.apod.title);
             Console.WriteLine("Date: " + apod_api.apod.date);
-            Console.WriteLine("Url: " + apod_api.apod.url);
+
+            int action;
+            Console.WriteLine("[1] Standard");
+            Console.WriteLine("[2] HD");
+            Console.WriteLine("[0] Don't download.");
+            Console.Write("Download? ");
+
+            action = int.Parse(Console.ReadLine());
+            WebClient dl = new WebClient();
+
+            switch (action)
+            {
+                case 0:
+                    Console.WriteLine("Photo will not be downloaded.");
+                    break;
+                case 1:
+                    Console.WriteLine("Downloading standard photo . . .");
+                    dl.DownloadFile(apod_api.apod.url, "apod_" + apod_api.apod.date + ".jpg");
+                    Console.WriteLine("Finished downloading.");
+                    break;
+                case 2:
+                    Console.WriteLine("Downloading hd photo . . .");
+                    dl.DownloadFile(apod_api.apod.hdurl, "apod_" + apod_api.apod.date + ".jpg");
+                    Console.WriteLine("Finished downloading.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    break;
+            }
         }
     }
 }
